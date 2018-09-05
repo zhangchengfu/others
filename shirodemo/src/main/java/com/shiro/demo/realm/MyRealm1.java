8e67cefef63bd66b49be4fe611689893b23a51f4
AmVilCres <jzaofox@foxmail.com>
2018-09-05 23:00:04 +0800

    shiro入门Demo

---
+++ shirodemo/src/main/java/com/shiro/demo/realm/MyRealm1.java
@@ -0,0 +1,29 @@
+package com.shiro.demo.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+public class MyRealm1 implements Realm {
+    public String getName() {
+        return "myrealm1";
+    }
+
+    public boolean supports(AuthenticationToken authenticationToken) {
+        // 仅支持UsernamePasswordToken类型的Token
+        return authenticationToken instanceof UsernamePasswordToken;
+    }
+
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String username = (String) authenticationToken.getPrincipal();//得到用户名
+        String password = new String((char[])authenticationToken.getCredentials()); // 得到密码
+        if(!"zhang".equals(username)){
+            throw new UnknownAccountException("该账户不存在");
+        }
+
+        if(!"123".equals(password)){
+            throw new IncorrectCredentialsException("密码不正确");
+        }
+        // 如果身份认证成功，返回一个AuthenticationInfo 实现
+        return new SimpleAuthenticationInfo(username,password,getName());
+    }
+}
