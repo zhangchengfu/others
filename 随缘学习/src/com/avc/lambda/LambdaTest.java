6584f0289693ed7f7e969e7063707ccb7389481c
“AmVilCresx <jzaofox@foxmail.com>
2019-01-05 16:31:08 +0800

    commit

--- 随缘学习/src/com/avc/lambda/LambdaTest.java
+++ 随缘学习/src/com/avc/lambda/LambdaTest.java
@@ -2,10 +2,14 @@ package com.avc.lambda;
 
 import org.testng.AssertJUnit;
 
+import com.avc.stream.StringCollector;
+import com.avc.stream.StringCombiner;
 import com.sun.org.apache.xerces.internal.impl.dv.ValidatedInfo;
 
 import org.testng.AssertJUnit;
 import static org.testng.AssertJUnit.assertEquals;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.LinkedList;
@@ -44,10 +48,12 @@ public class LambdaTest {
     	System.out.println("max = "+toger);
     	// 最小值
     	String min = c2.stream().min(Comparator.comparing(s -> s.length())).get();
-    	System.out.println(min);
+    	System.out.println("min="+min);
     	// 求和
-    	int count = Stream.of(1,2,3).reduce(0, (acc,ele) -> acc+ele);
-    	System.out.println(count);
+    	//简写： Stream.of(1,2,3).reduce(0, (acc,ele) -> acc+ele);
+    	int count = Stream.of(1,2,3).reduce(0, (acc,ele) -> { return acc+ele;});
+    	count = Stream.of(1,2,3).reduce(0, Integer::sum);
+    	System.out.println("count = "+count);
     	// 流式求和
     	Integer[] ar = new Integer[]{1,3,3};
     	System.out.println(addUp(Stream.of(ar)));
@@ -57,35 +63,34 @@ public class LambdaTest {
     	
     	// 数据分块(分解成两个集合)
     	Map<Boolean, List<String>> ls = c2.stream().collect(Collectors.partitioningBy(s -> s.length() > 1));
-    	/*for(Map.Entry<Boolean, List<String>> m : ls.entrySet()) {
-    		System.out.println(m.getKey());
-    		System.out.println(Arrays.deepToString(m.getValue().toArray()));
-    	}*/
-    	
+    /*	System.out.println("数据分块");
+    	ls.forEach((k, v) ->{
+    		System.out.println(k + "---" + Arrays.deepToString(v.toArray()));
+    	});
+    	*/
     	// 数据分组是一种更自然的分割数据操作，可以使用任意值对数据分组
     	Map<Integer, List<String>> map =  c2.stream().collect(Collectors.groupingBy(s -> s.length()));
     	/*System.out.println("数组分组：");
-    	for(Map.Entry<Integer, List<String>> m : map.entrySet()) {
-    		System.out.println(m.getKey());
-    		System.out.println(Arrays.deepToString(m.getValue().toArray()));
-    	}*/
+    	map.forEach((k,v) -> {
+    		System.out.println(k);
+    		System.out.println(Arrays.deepToString(v.toArray()));
+    	});*/
     	
     	// 组合收集器
     	Map<Integer, Long> cm = c2.stream().collect(Collectors.groupingBy(s -> s.length(),Collectors.counting()));
-    	System.out.println("组合收集器：");
-    	/*for(Map.Entry<Integer, Long> m : cm.entrySet()) {
-    		System.out.println(m.getKey()+"::"+m.getValue());
-    	}*/
+    /*	System.out.println("组合收集器：");
     	Map<Integer, List<String>> p = c2.stream().collect(Collectors.groupingBy(s -> s.length(),
     			 Collectors.mapping(val -> val.toLowerCase(), Collectors.toList())));
-    	
-    	for(Map.Entry<Integer, List<String>> m : p.entrySet()) {
-    		System.out.println(m.getKey()+"::"+m.getValue());
-    	}
-    	
+    	cm.forEach((key,value)->{
+    		System.out.println(key+"::"+value);
+    	});
+    	*/
     	// 字符串
     	String str = c2.stream().collect(Collectors.joining(",", "[", "]"));
     	//System.out.println("字符串："+str);
+    	//stringCombinerTest();
+    	List<Integer> nums = Stream.of(1,2,3,4,5).collect(Collectors.toList());
+    	System.out.println("***:"+multiplyThrough(nums));
     }
     
 	public long externalCountArtistsFromLondonExpanded(List<String> allArtists) {
@@ -96,4 +101,27 @@ public class LambdaTest {
 		 return numbers.reduce((acc,ele)-> acc+ele).get();
 	 }
    
+	private static void stringCombinerTest() {
+		List<String> strList = new ArrayList<>();
+		strList.add("Acb");
+		strList.add("fff");
+		strList.add("xYz");
+		
+		/*String res = strList.stream()
+				.map(s->s.toLowerCase())
+				.reduce(new StringCombiner(",", "[", "]"),
+						StringCombiner::add,
+						StringCombiner::merge)
+		.toString();*/
+		
+		String res = strList.stream()
+				.map(s->s.toLowerCase())
+				.collect(new StringCollector(",","[","]"));
+		
+		System.out.println("res = "+res);
+	}
+	
+	private static int multiplyThrough(List<Integer> nums) {
+		return 5*nums.parallelStream().reduce(1, (acc,x) -> acc * x);
+	}
 }
